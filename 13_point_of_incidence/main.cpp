9922acd1ccaa7d7247a80f54bc85cc3f5ab69116
KarolOrszulik <orkarox@gmail.com>
2023-12-13 21:04:26 +0100

    Pt 2 complete

--- 13_point_of_incidence/main.cpp
+++ 13_point_of_incidence/main.cpp
@@ -43,8 +43,10 @@ std::vector<Map> load_maps_from_file(const std::string& path)
     return result;
 }
 
-size_t lines_above_h_reflection(const Map& map)
+std::vector<size_t> lines_above_h_reflection(const Map& map)
 {
+    std::vector<size_t> result;
+
     for (size_t above = map.size() - 1; above > 0; above--)
     {
         size_t rows_to_check = std::min(above, map.size() - above);
@@ -63,14 +65,20 @@ size_t lines_above_h_reflection(const Map& map)
                 }
         }
         if (match)
-            return above;
+            result.push_back(above);
     }
 
-    return 0;
+    if (result.empty())
+        result.push_back(0);
+
+    return result;
 }
 
-size_t lines_left_of_v_reflection(const Map& map)
+std::vector<size_t> lines_left_of_v_reflection(const Map& map)
 {
+
+    std::vector<size_t> result;
+
     for (size_t to_left = map[0].size() - 1; to_left > 0; to_left--)
     {
         size_t cols_to_check = std::min(to_left, map[0].size() - to_left);
@@ -89,10 +97,13 @@ size_t lines_left_of_v_reflection(const Map& map)
                 }
         }
         if (match)
-            return to_left;
+            result.push_back(to_left);
     }
 
-    return 0;
+    if (result.empty())
+        result.push_back(0);
+
+    return result;
 }
 
 size_t part_1(const std::vector<Map>& maps)
@@ -100,14 +111,11 @@ size_t part_1(const std::vector<Map>& maps)
     size_t result = 0;
 
     for (const auto& m : maps)
-        result += 100 * lines_above_h_reflection(m) + lines_left_of_v_reflection(m);
+        result += 100 * lines_above_h_reflection(m).front() + lines_left_of_v_reflection(m).front();
 
     return result;
 }
 
-// the result of this function is not correct
-// partly because I don't think I understand what
-// exactly to do
 size_t part_2(const std::vector<Map>& maps)
 {
     size_t result = 0;
@@ -115,34 +123,36 @@ size_t part_2(const std::vector<Map>& maps)
     size_t _counter = 0;
     for (const auto& m : maps)
     {
-        _counter++;
-        size_t original_a = lines_above_h_reflection(m);
-        size_t original_l = lines_left_of_v_reflection(m);
-
-        Map swapped = m;
+        size_t original_a = lines_above_h_reflection(m)[0];
+        size_t original_l = lines_left_of_v_reflection(m)[0];
 
         bool found = false;
-        for (size_t y = 0; y < swapped.size() && !found; y++)
+        for (size_t y = 0; y < m.size() && !found; y++)
         {
-            for (size_t x = 0; x < swapped[y].size(); x++)
+            for (size_t x = 0; x < m[y].size() && !found; x++)
             {
+
+                Map swapped = m;
                 swapped[y][x] = !swapped[y][x];
 
-                size_t new_a = lines_above_h_reflection(swapped);
-                size_t new_l = lines_left_of_v_reflection(swapped);
+                size_t new_a = lines_above_h_reflection(swapped).front();
+                if (new_a == original_a)
+                    new_a = lines_above_h_reflection(swapped).back();
 
-                if (new_a != 0 && new_a != original_a || new_l != 0 && new_l != original_l)
+                size_t new_l = lines_left_of_v_reflection(swapped).front();
+                if (new_l == original_l)
+                    new_l = lines_left_of_v_reflection(swapped).back();
+
+                bool found_smudge = (new_a != 0 && new_a != original_a) || (new_l != 0 && new_l != original_l);
+
+                if (found_smudge)
                 {
+                    found = true;
                     if (original_a != new_a)
                         result += 100 * new_a;
                     else
                         result += new_l;
-
-                    found = true;
-                    break;
                 }
-                
-                swapped[y][x] = !swapped[y][x];
             }
         }
     }
